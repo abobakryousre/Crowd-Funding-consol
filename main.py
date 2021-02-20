d1f2ba9b7895f595b2d947d31fd65d9aa4025d59
Abobakr <abobakryousre@gmail.com>
2021-02-20 16:12:53 +0200

    main menu

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from library import validator
 
 
-def main():
+def display_main_menu():
+    logedin = False
+    print("1) Login")
+    print("2) Sign Up ")
+    while not logedin:
+        try:
+            option = int(input("enter your option: "))
+        except:
+            print("Your option must be a number! ")
+        else:
+            if option == 1:
+                load_login_page()
+                logedin = True
+            elif option == 2:
+                load_sginup_page()
+
+
+def load_login_page():
     pass
+
+
+def load_sginup_page():
+    pass
+
+
+def main():
+    display_main_menu()
 
 
 if __name__ == '__main__':
     main()
-
