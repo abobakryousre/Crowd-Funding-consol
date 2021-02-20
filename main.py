3b067f413ebbb5a8a22a0874ce5173b2beac5a88
Abobakr <abobakryousre@gmail.com>
2021-02-20 17:42:28 +0200

    add  login function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from library import validator
 import json
 
+usr_information = {}
+
 
 def display_main_menu():
-    logedin = False
-    print("1) Login")
-    print("2) Sign Up ")
-    while not logedin:
+    login = False
+
+    while not login:
+        print("1) Login")
+        print("2) Sign Up ")
         try:
             option = int(input("enter your option: "))
         except:
@@ -14,13 +17,9 @@ def display_main_menu():
         else:
             if option == 1:
                 load_login_page()
-                logedin = True
+                login = True
             elif option == 2:
                 load_sginup_page()
-
-
-def load_login_page():
-    pass
 
 
 def load_sginup_page():
@@ -52,8 +51,6 @@ def load_sginup_page():
             continue
         else:
             print("Sign up successfully ")
-            print("1) Login")
-            print("2) Sign Up ")
             add_new_user(first_name, last_name, email, password, mobile)
             signedup = True
 
@@ -77,10 +74,44 @@ def add_new_user(first_name, last_name, email, password, mobile):
         json.dump(all_user, users)
 
 
+def load_login_page():
+    logedin = False
+    while not logedin:
+        username = input("email: ")
+        password = input("Password: ")
+        if not valid_user(username, password):
+            print("Invalid Username or Password, Please try again..")
+        else:
+            logedin = True
+            load_project_pages()
+
+
+def valid_user(username, password):
+    valid = False
+    global usr_information
+
+    with open("users.txt", "r") as users:
+        all_users = json.load(users)
+        for user in all_users:
+            if username == user.get("email"):
+                if password == user.get("password"):
+                    valid = True
+                    usr_information = user
+                    break
+
+    if valid:
+        return True
+    else:
+        return False
+
+
+def load_project_pages():
+    pass
+
+
 def main():
     display_main_menu()
 
 
 if __name__ == '__main__':
     main()
-
